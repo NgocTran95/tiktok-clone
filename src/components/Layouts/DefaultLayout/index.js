a2d2ef78e15326376b55993ba8281a32b868c3de
ngocbk <ngoctranbk95@gmail.com>
2022-09-14 16:24:17 +0700

    set Default Layout

--- src/components/Layouts/DefaultLayout/index.js
+++ src/components/Layouts/DefaultLayout/index.js
@@ -1,13 +1,18 @@
+import classNames from 'classnames/bind';
+
+import styles from './DefaultLayout.module.scss'
 import Header from '~/components/Layouts/components/Header';
 import Sidebar from './Sidebar';
 
+const cx = classNames.bind(styles)
+
 function DefaultLayout({ children }) {
     return (
-        <div>
+        <div className={cx('wrapper')}> 
             <Header />
-            <div className="Container">
+            <div className={cx('container')}>
                 <Sidebar />
-                <div className="Content">{children}</div>
+                <div className={cx('content')}>{children}</div>
             </div>
         </div>
     );
